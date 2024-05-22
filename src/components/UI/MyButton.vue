ea203c1ccab4c53c64703580ca2837dbb0672da8
ElenaVereskun <lena.vereskun@yandex.ru>
2024-05-22 11:55:42 +0300

    [-]addPost

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+};
+</script>
+
+<style scoped>
+.btn {
+  display: flex;
+  padding: 10px 30px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <input :value="modelValue" @input="updateInput" class="input" type="text" />
+</template>
+
+<script>
+export default {
+  name: "my-input",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 30px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
